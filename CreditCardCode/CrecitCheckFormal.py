9d440468e86995f66323676844890501c612e536
mmbc2008 <m-b-c@mail.com>
2020-09-30 23:12:47 +0200

    Added comments to the current methods and added a validation function, along with the main function.

--- CreditCardCode/CrecitCheckFormal.py
+++ CreditCardCode/CrecitCheckFormal.py
@@ -1,32 +1,91 @@
-#def main():
-CreditCardNum = (str(input("give your creditcard number with spaces ")))
-num = list(map(int, CreditCardNum.split()))
-print(num)
+"""
+This function asks the user for their credit card number and tells them whether or not it is valid.
+"""
 
 
-def length_card(num):
-    if num >= 13 and num <= 16:
-        if num [0] == "4" or num [0] == "5" or num [0] == "6" or (num [0] == "3" and num [1] == "7"):
+def ask_user():
+    string = (str(input("Please input your credit card number\n")))
+    numbers = list(map(int, string.split()))
+    if card_length(numbers):
+        validation(numbers)
+    else:
+        print("The credit card number you entered is invalid")
+
+
+"""
+This function takes the credit card number as a parameter and checks the length of the credit card number 
+to see if it is valid or not.
+"""
+
+
+def card_length(num):
+    length = len(num)
+    if 13 <= length <= 16:
+        if num[0] == "4" or num[0] == "5" or num[0] == "6" or (num[0] == "3" and num[1] == "7"):
             return True
     else:
         return False
 
 
+"""
+This method takes the list of numbers and tells the user if it is a valid combination with a print statement
+"""
+
+
+def validation(numbers):
+    odd_results = odd_digits(numbers)
+    even_results = even_digits(numbers)
+    sum_of_results = odd_results + even_results
+    if sum_of_results % 10 == 0:
+        print("This credit card number is valid")
+    else:
+        print("This credit card number is invalid")
+
+
+"""
+This function takes the credit card number as a string list parameter and adds all
+of the even digits in it.
+"""
+
+
 def even_digits(num):
     length = len(num)
-    sum = 0
-    for i in range(length-2,-1,-2):
+    total = 0
+    for i in range(length - 2, -1, -2):
         number = eval(num[i])
         number = number * 2
         if number > 9:
             strNumber = str(number)
             number = eval(strNumber[0] + eval(strNumber[1]))
-            sum += number
-        return sum
+            total += number
+        return total
+
+
+"""
+This function takes the credit card number as a string list parameter and adds all
+of the odd digits in it.
+"""
+
 
 def odd_digits(num):
     length = len(num)
-    SumOdd = 0
-    for i in range(length-1, -1, -2):
-        num += SumOdd
-    return SumOdd
+    sumOdd = 0
+    for i in range(length - 1, -1, -2):
+        num += sumOdd
+    return sumOdd
+
+
+"""
+This is the main function that defines our first function called ask_user
+"""
+
+
+def main():
+    ask_user()
+
+
+"""
+This calls the main function
+"""
+if __name__ == '__main__':
+    main()
