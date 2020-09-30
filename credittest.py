49c0abdf540c545a9a3784b2747d6d05178a8188
bosduco <d.v.vanden.bos@student.vu.nl>
2020-09-30 17:46:37 +0200

    Now the code makes more sense with the pseudocode

--- credittest.py
+++ credittest.py
@@ -1,28 +1,29 @@
 credit_card_number = input("enter your credit card number>>")
-digits = [int(digit) for digit in str(credit_card_number)] #x are the individual numbers
+digits = [int(digit) for digit in str(credit_card_number)]
 digits.reverse()
-print(digits) #to check if the previous step went okay
+print("this is the reversed input ", digits) #to check if the step went correctly
 
 #step 1 from assignment
 added_numbers = 0
 evendigits = digits[1::2]
-print(evendigits)  #to check if the previous step went okay
+print('this is the list of even digits from the input ', evendigits)  #to check if the previous step went okay
 for digit in evendigits:
     number1 = digit*2
     number1 = sum(int(digit) for digit in str(number1))
     #step 2
     added_numbers = added_numbers+number1
-print(added_numbers) #to check if the step went correctly
+print('these are the added values of step 2: ', added_numbers) #to check if the step went correctly
 
 #step 3 from assignment
 odd_digits = digits[::2]
+print('this is the list of odd digits from the input ',odd_digits) #to check if the step went correctly
 number2 = sum(odd_digits)
-print(number2) #to check if the step went correctly
+print('these are the added values of all odd digits: ', number2) #to check if the step went correctly
 
 
 #step 4 from assignment
 total_numbers = number2 + added_numbers
-print(total_numbers)
+print('this is the sum of the odd and the even digits: ', total_numbers) #to check if the step went correctly
 
 if total_numbers % 10 == 0:
     print('This is a valid credit card number')
